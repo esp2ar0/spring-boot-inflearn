9a7f22820e4dca435e2f82ca51a05a74b9cdad80
AlgoDesk07\AlgoUser <esp2ar0@gmail.com>
2019-01-22 17:46:24 +0900

    4부 스프링 부트 활용 - 테스트

---
+++ springtestdemo/src/main/java/com/esp2ar0/springtestdemo/sample/SampleService.java
@@ -0,0 +1,11 @@
+package com.esp2ar0.springtestdemo.sample;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleService {
+
+    public String getName() {
+        return "ChangHwan";
+    }
+}
