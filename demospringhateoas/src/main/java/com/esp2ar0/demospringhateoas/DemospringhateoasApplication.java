c8b57de0845e0282cfca64e587f19151562d896c
AlgoDesk07\AlgoUser <esp2ar0@gmail.com>
2019-02-01 17:54:28 +0900

    4부 - 스프링 웹 MVC 11부:CORS 까지

---
+++ demospringhateoas/src/main/java/com/esp2ar0/demospringhateoas/DemospringhateoasApplication.java
@@ -0,0 +1,14 @@
+package com.esp2ar0.demospringhateoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemospringhateoasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemospringhateoasApplication.class, args);
+    }
+
+}
+
---
+++ demospringhateoas/src/test/java/com/esp2ar0/demospringhateoas/DemospringhateoasApplicationTests.java
@@ -0,0 +1,17 @@
+package com.esp2ar0.demospringhateoas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemospringhateoasApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
