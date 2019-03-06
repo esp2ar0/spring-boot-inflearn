d30a92645adaee740e8762cb90f4c082d476f72a
ChangHwan-Lee <esp2ar0@gmail.com>
2019-03-06 18:35:19 +0900

    ?
    ldd
    
    dpwe
    q
    
    ;;
    ;

--- demo-jpa/src/test/java/com/esp2ar0/demojpa/post/PostRepositoryTest.java
+++ demo-jpa/src/test/java/com/esp2ar0/demojpa/post/PostRepositoryTest.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -16,7 +18,18 @@ public class PostRepositoryTest {
 	PostRepository postRepository;
 
 	@Test
-	public void crud() {
+	public void update() {
+		Post post = new Post();
+		post.setTitle("jpa");
+		postRepository.save(post);
+
+		List<Post> all = postRepository.findAll();
+		assertThat(all.get(0).getTitle()).isEqualTo("jpa");
+
+		post.setTitle("hibernate");
+
+		List<Post> all2 = postRepository.findAll();
+		assertThat(all2.get(0).getTitle()).isEqualTo("hibernate");
 	}
 
 }
